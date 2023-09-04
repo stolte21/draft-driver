191bab9566297ef904ea9a83efaf152308518ef4
Ross Stolte <rossstolte@gmail.com>
2023-09-04 11:09:03 -0400

    add bottom padding in page loader

--- components/PageLoader.tsx
+++ components/PageLoader.tsx
@@ -2,7 +2,13 @@ import { Flex, Skeleton } from '@chakra-ui/react';
 
 const PageLoader = () => {
   return (
-    <Flex height="100%" width="100%" flexDirection="column" gap={2}>
+    <Flex
+      height="100%"
+      width="100%"
+      flexDirection="column"
+      gap={2}
+      paddingBottom={2}
+    >
       <Flex flexDirection={['column', 'row']} gap={[1, 2]}>
         {[1, 2].map((_) => (
           <Skeleton key={_} height="40px" width="100%" />
