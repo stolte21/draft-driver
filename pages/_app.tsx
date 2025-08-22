47aed1719aa3f321888b1954e9e0af55c4cf1ea1
Ross Stolte <rossstolte@gmail.com>
2025-08-21 22:25:57 -0400

    fix: move DraftProvider to _app.tsx for better context management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ChakraProvider from 'providers/ChakraProvider';
 import SettingsProvider from 'providers/SettingsProvider';
+import DraftProvider from 'providers/DraftProvider';
 
 const queryClient = new QueryClient();
 
@@ -9,7 +10,9 @@ const App = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider>
     <QueryClientProvider client={queryClient}>
       <SettingsProvider>
-        <Component {...pageProps} />
+        <DraftProvider>
+          <Component {...pageProps} />
+        </DraftProvider>
       </SettingsProvider>
     </QueryClientProvider>
   </ChakraProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,16 @@ import AppBar from 'components/AppBar';
 import Roster from 'components/Roster';
 import Toolbar from 'components/Toolbar';
 import DraftBoard from 'components/DraftBoard';
-import DraftProvider from 'providers/DraftProvider';
 
 const Index: NextPage = () => {
   return (
     <Page>
       <AppBar />
-      <DraftProvider>
-        <Grid paddingY={2} gridColumn={2} height="100%" templateRows="auto 1fr">
-          <Toolbar />
-          <DraftBoard />
-        </Grid>
-        <Roster />
-      </DraftProvider>
+      <Grid paddingY={2} gridColumn={2} height="100%" templateRows="auto 1fr">
+        <Toolbar />
+        <DraftBoard />
+      </Grid>
+      <Roster />
     </Page>
   );
 };
