a404767ba5043c2cc3ba103e6a22cbce1b859d2c
Ross Stolte <rossstolte@gmail.com>
2025-08-23 18:37:01 -0400

    fix: add back color mode script to set dark mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,11 @@
+import { ColorModeScript } from '@chakra-ui/react';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => (
   <Html>
     <Head />
     <body>
+      <ColorModeScript initialColorMode="dark" />
       <Main />
       <NextScript />
     </body>
